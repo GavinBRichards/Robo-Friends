a68140019f9a7c8cf29d8b4dc8504edce6c271d9
Gavin Richards <hillarygavin@gmail.com>
2019-11-09 11:43:54 -0700

    Updates

---
+++ precache-manifest.dc82422c13c25066092806d361c946cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de7672882baaef014af50c78bb48cfa",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "34cba0c1014474108e3d",
+    "url": "/Robo-Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ab20c51211691c05d624",
+    "url": "/Robo-Friends/static/css/main.dbb18e90.chunk.css"
+  },
+  {
+    "revision": "34cba0c1014474108e3d",
+    "url": "/Robo-Friends/static/js/2.7b7c49bb.chunk.js"
+  },
+  {
+    "revision": "ab20c51211691c05d624",
+    "url": "/Robo-Friends/static/js/main.90be031b.chunk.js"
+  },
+  {
+    "revision": "18ddf7fcd750b04b005f",
+    "url": "/Robo-Friends/static/js/runtime-main.602c1e8e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
